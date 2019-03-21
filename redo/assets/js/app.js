2875c485a6e348b940524d4e9a3717a1b4bf3a92
brennas22 <brenna.sorkin@gmail.com>
2019-03-21 12:40:54 -0400

    edit timeblocks

--- redo/assets/js/app.js
+++ redo/assets/js/app.js
@@ -9,30 +9,47 @@ import css from "../css/app.scss"
 //
 // Import dependencies
 //
-import "phoenix_html"
+
 import jQuery from 'jquery';
 window.jQuery = window.$ = jQuery; // Bootstrap requires a global "$" object.
 import "bootstrap";
 
-(function () {
-  "use strict";
+import "phoenix_html"
 
-  let start_time = null;
-  let stop_time = null;
+$(function () {
 
-  document.getElementById("start_button").addEventListener("click", function () { start(); });
-  document.getElementById("end_button").addEventListener("click", function () { end(); });
+  $('#end_button').hide();
+  $('.save-button').hide();
 
+  let start_time = 0;
+  let stop_time = 0;
 
-  function start() {
+  $('#start_button').click(() => {
     start_time = new Date();
     var starttime = document.getElementById("start_time");
     starttime.innerHTML = "timer running";
-  }
+    $('#start_button').hide();
+    $('#end_button').show();
 
-  function end() {
+
+  });
+
+  $('.edit-button').click((ev) => {
+    let show_save = $(ev.target).siblings(".save-button").show();
+    $(ev.target).hide();
+
+    let td = $(ev.target).closest(".editable-row");
+    let endcell = td.find("#end-field").prop( "disabled", false );
+    let startcell = td.find("#start-field").prop( "disabled", false );
+
+  });
+
+  $('#end_button').click((ev) => {
     stop_time = new Date();
-    console.log(window.task_id);
+    var starttime = document.getElementById("start_time");
+    starttime.innerHTML = "timer stopped";
+
+    let task_id = $(ev.target).data("task_id");
 
     $.ajax("/ajax/time_blocks", {
       method: "post",
@@ -42,7 +59,7 @@ import "bootstrap";
         time_block: {
           start: start_time,
           end: stop_time,
-          task_id: window.task_id,
+          task_id: task_id,
         }
       }),
       success: (resp) => {location.reload()},
@@ -50,12 +67,38 @@ import "bootstrap";
       console.error(err);
     }
     });
-    console.log(start_time + ", " + stop_time);
-  }
+  });
 
+  $('.save-button').click((ev) => {
+    let timeblock = $(ev.target).data("timeblock")
+    let td = $(ev.target).closest(".editable-row");
+    let endcell = td.find("#end-field").prop( "disabled", true );
+    let startcell = td.find("#start-field").prop( "disabled", true );
 
+    let show_edit = $(ev.target).siblings(".edit-button").show();
+    $(ev.target).hide();
 
-})();
+    let task_id = $(ev.target).data("task_id");
+
+    $.ajax("/ajax/time_blocks/" + timeblock, {
+      method: "put",
+      datatype: "json",
+      contentType: "application/json; charset=UTF-8",
+      data: JSON.stringify({
+        time_block: {
+          start: $(startcell).val(),
+          end: $(endcell).val(),
+          task_id: task_id,
+        }
+      }),
+      success: (resp) => {},
+      error: (err) => {
+      console.error(err);
+    }
+    });
+  });
+
+});
 
 
 // Import local files
